41ed80cfb3c1e35b81b5f3c7477de270fa997eec
BradleyTim <timbradley94@gmail.com>
2020-04-30 19:47:19 +0300

    update view to display errors

--- resources/views/blog/create.blade.php
+++ resources/views/blog/create.blade.php
@@ -6,15 +6,24 @@
     <h2>Create A New Blog Post</h2>
     <div class="form-group">
       <label for="title">Title</label>
-      <input type="text" name="title" id="title" class="form-control" placeholder="Title">
+      <input type="text" name="title" id="title" class="form-control" placeholder="Title" value="{{ old('title') }}">
+      @error('title')
+        <div class="alert-error">{{ $message }}</div>
+      @enderror
     </div>
     <div class="form-group">
       <label for="slug">Slug</label>
-      <input type="text" name="slug" id="slug" class="form-control" placeholder="Slug">
+      <input type="text" name="slug" id="slug" class="form-control" placeholder="Slug" value="{{ old('slug') }}">
+      @error('slug')
+        <div class="alert-error">{{ $message }}</div>
+      @enderror
     </div>
     <div class="form-group">
       <label for="body">Body</label>
-      <textarea name="body" id="body" class="form-control" placeholder="Body" cols="30" rows="5"></textarea>
+      <textarea name="body" id="body" class="form-control" placeholder="Body" cols="30" rows="5">{{ old('body') }}</textarea>
+      @error('body')
+        <div class="alert-error">{{ $message }}</div>
+      @enderror
     </div>
     <button type="submit" class="btn btn-primary">Create Blog</button>
   </form>
