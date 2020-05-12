86e7954c6417de40285c922502d17e000f780a8b
BradleyTim <timbradley94@gmail.com>
2020-05-12 12:52:45 +0300

    add bootstrap css cdn and alpinejs cdn

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,11 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
         <!-- Styles -->
-    <link rel="stylesheet" href="/css/app.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        {{-- <link rel="stylesheet" href="/css/app.css"> --}}
+
+        {{-- Scripts --}}
+        <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
     </head>
     <body>
         <div class="container flex-layout">
