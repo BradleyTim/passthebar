c06937c927ed3fdee3064d95d4f834e9a11173a0
BradleyTim <timbradley94@gmail.com>
2020-05-20 08:18:08 +0300

    add breadcrumbs menu to home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,8 +2,15 @@
 
 @section('content')
 <div class="container">
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="{{ route('blog.index')}}">Home</a></li>
+            <li class="breadcrumb-item"><a href="{{ route('blog.create') }}">Blogs</a></li>
+            <li class="breadcrumb-item"><a href="{{ route('tags.create') }}">Tags</a></li>
+        </ol>
+    </nav>
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
                 <div class="card-header">Dashboard</div>
 
@@ -19,10 +26,16 @@
             </div>
             <div class="card mt-3">
                 <div class="card-header">Blog Posts</div>
-
                 <div class="card-body">
                     <h4>Create a new Blog</h4>
-                <a class="btn btn-primary btn-sm" href="{{ route('blog.create')}}">Create</a>
+                    <a class="btn btn-primary btn-sm" href="{{ route('blog.create')}}">Create a Blog</a>
+                </div>
+            </div>
+            <div class="card mt-3">
+                <div class="card-header">Tags</div>
+                <div class="card-body">
+                    <h4>Create a new Tag</h4>
+                    <a class="btn btn-primary btn-sm" href="{{ route('tags.create')}}">Create a Tag</a>
                 </div>
             </div>
         </div>
