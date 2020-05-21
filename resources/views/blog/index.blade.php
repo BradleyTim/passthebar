ae8dd74f3f67008818e76ccb7b45fd159da34d5f
BradleyTim <timbradley94@gmail.com>
2020-05-21 16:31:42 +0300

    update blog index page styles and UI

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -2,10 +2,14 @@
 
 @section('content')
   @forelse ($blogs as $blog)
-    <div class="card pl-2 py-2 mb-2">
-      <h4 class="font-weight-light text-capitalize">
-        <a class="text-sm text-secondary text-decoration-none" href="{{ route('blog.show', $blog->id) }}">{{ $blog->title }}</a> 
-      </h4>  
+    <div class="blog-list">
+      <h4 class="font-weight-bold text-capitalize">
+        <a class="text-decoration-none text-black" href="{{ route('blog.show', $blog->id) }}">{{ $blog->title }}</a> 
+      </h4>
+      @foreach ($blog->tags as $tag)
+        <a class="text-sm text-secondary text-decoration-none pr-2" href="/blog?tag={{ $tag->name }}">#{{ $tag->name }}</a>
+      @endforeach
+      <div class="text-sm text-secondary text-decoration-none mt-2">{{ $blog->created_at->format('M d') }}</div>
     </div>
   @empty
     <p class="display-4 text-muted center">It's empty in here for now. No relevant articles yet.</p> 
