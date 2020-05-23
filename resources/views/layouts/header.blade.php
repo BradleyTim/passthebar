8eafe2079e3886f270a98cee314078ac4d0b9f40
BradleyTim <timbradley94@gmail.com>
2020-05-23 23:09:16 +0300

    add dashboard route

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -33,7 +33,7 @@
                   @endif
               @else
                   <li class="nav-item">
-                      <a class="nav-link" href="/blog/create">Create</a>
+                      <a class="nav-link" href="/home">Dashboard</a>
                   </li>
                   <li class="nav-item dropdown">
                       <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
