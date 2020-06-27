1f029d1ace0a1713822c216de2a66054b54588a3
BradleyTim <timbradley94@gmail.com>
2020-06-27 20:44:17 +0300

    update about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,7 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="title m-b-md">
-        About Us
+    <div class="title m-b-md about-page">
+        <h3>About PTB</h3>
+        <p>
+            We are a platform for promoting justice, integrity and the rule of law.
+            We believe in transparency and adding value to the legal ecosystem. 
+            We hope you poke around and like what you see!
+        </p>
     </div>
 @endsection
